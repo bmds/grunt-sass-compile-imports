b5c855722845fc2c40b77fe483079c5b0856e020
Barney Scott <github@barnabyscott.co.uk>
2015-05-29 10:09:20 +0100

    Convert indentation to tabs to neaten up the files

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,102 +9,102 @@
 'use strict';
 
 var filesConfig = [{
-    expand: true,
-    cwd   : 'test/fixtures/',
-    src   : ['**/*.scss']
+	expand: true,
+	cwd:    'test/fixtures/',
+	src:    ['**/*.scss']
 }];
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>',
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			},
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp'],
+		},
 
-    // Configuration to be run (and then tested).
-    sass_compile_imports: {
-      default_options: {
-        target: 'tmp/_default.scss',
-        files: filesConfig
-      },
-      keep_extension: {
-        options: {
-            removeExtension: false
-        },
-        target: 'tmp/_with-extension.scss',
-        files: filesConfig
-      },
-      replace_path: {
-        options: {
-            replacePath: {
-                pattern: 'test/fixtures',
-                replace: '../_styles'
-            }
-        },
-        target: 'tmp/_replaced-path.scss',
-        files: filesConfig
-      },
-      import_path: {
-        options: {
-          importPath: '../another/location/'
-        },
-        target: 'tmp/_import-path.scss',
-        files: filesConfig
-      },
-      import_path_no_slash: {
-        options: {
-          importPath: '../another/location'
-        },
-        target: 'tmp/_import-path-slash.scss',
-        files: filesConfig
-      },
-      custom_header: {
-        options: {
-          header: [
-            '// Custom header',
-            '// DO NOT TOUCH',
-            ''
-          ]
-        },
-        target: 'tmp/_custom-header.scss',
-        files: filesConfig
-      }
-    },
+		// Configuration to be run (and then tested).
+		sass_compile_imports: {
+			default_options: {
+				target: 'tmp/_default.scss',
+				files:  filesConfig
+			},
+			keep_extension: {
+				options: {
+					removeExtension: false
+				},
+				target: 'tmp/_with-extension.scss',
+				files:  filesConfig
+			},
+			replace_path: {
+				options: {
+					replacePath: {
+						pattern: 'test/fixtures',
+						replace: '../_styles'
+					}
+				},
+				target: 'tmp/_replaced-path.scss',
+				files:  filesConfig
+			},
+			import_path: {
+				options: {
+					importPath: '../another/location/'
+				},
+				target: 'tmp/_import-path.scss',
+				files:  filesConfig
+			},
+			import_path_no_slash: {
+				options: {
+					importPath: '../another/location'
+				},
+				target: 'tmp/_import-path-slash.scss',
+				files:  filesConfig
+			},
+			custom_header: {
+				options: {
+					header: [
+						'// Custom header',
+						'// DO NOT TOUCH',
+						''
+					]
+				},
+				target: 'tmp/_custom-header.scss',
+				files:  filesConfig
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js'],
+		}
 
-  });
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-debug-task');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-debug-task');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'sass_compile_imports', 'nodeunit']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'sass_compile_imports', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint']);
 
 };
--- test/sass_compile_imports_test.js
+++ test/sass_compile_imports_test.js
@@ -3,78 +3,78 @@
 var grunt = require('grunt');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+	======== A Handy Little Nodeunit Reference ========
+	https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+	Test methods:
+		test.expect(numAssertions)
+		test.done()
+	Test assertions:
+		test.ok(value, [message])
+		test.equal(actual, expected, [message])
+		test.notEqual(actual, expected, [message])
+		test.deepEqual(actual, expected, [message])
+		test.notDeepEqual(actual, expected, [message])
+		test.strictEqual(actual, expected, [message])
+		test.notStrictEqual(actual, expected, [message])
+		test.throws(block, [error], [message])
+		test.doesNotThrow(block, [error], [message])
+		test.ifError(value)
 */
 
 exports.sass_compile_imports = {
-  default_options: function(test) {
-    test.expect(1);
+	default_options: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_default.scss');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'Default options should function correctly.');
+		var actual = grunt.file.read('tmp/_default.scss');
+		var expected = grunt.file.read('test/expected/default_options');
+		test.equal(actual, expected, 'Default options should function correctly.');
 
-    test.done();
-  },
-  keep_extension: function(test) {
-    test.expect(1);
+		test.done();
+	},
+	keep_extension: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_with-extension.scss');
-    var expected = grunt.file.read('test/expected/keep_extension');
-    test.equal(actual, expected, 'Extensions should be kept.');
+		var actual = grunt.file.read('tmp/_with-extension.scss');
+		var expected = grunt.file.read('test/expected/keep_extension');
+		test.equal(actual, expected, 'Extensions should be kept.');
 
-    test.done();
-  },
-  replace_path: function(test) {
-    test.expect(1);
+		test.done();
+	},
+	replace_path: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_replaced-path.scss');
-    var expected = grunt.file.read('test/expected/replace_path');
-    test.equal(actual, expected, 'Path replaced with setting.');
+		var actual = grunt.file.read('tmp/_replaced-path.scss');
+		var expected = grunt.file.read('test/expected/replace_path');
+		test.equal(actual, expected, 'Path replaced with setting.');
 
-    test.done();
-  },
-  import_path: function(test) {
-    test.expect(1);
+		test.done();
+	},
+	import_path: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_import-path.scss');
-    var expected = grunt.file.read('test/expected/import_path');
-    test.equal(actual, expected, 'Import paths should be used');
+		var actual = grunt.file.read('tmp/_import-path.scss');
+		var expected = grunt.file.read('test/expected/import_path');
+		test.equal(actual, expected, 'Import paths should be used');
 
-    test.done();
-  },
-  import_path_slash: function(test) {
-    test.expect(1);
+		test.done();
+	},
+	import_path_slash: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_import-path-slash.scss');
-    var expected = grunt.file.read('test/expected/import_path');
-    test.equal(actual, expected, 'Always add a trailing slash if one isn\'t set');
+		var actual = grunt.file.read('tmp/_import-path-slash.scss');
+		var expected = grunt.file.read('test/expected/import_path');
+		test.equal(actual, expected, 'Always add a trailing slash if one isn\'t set');
 
-    test.done();
-  },
-  custom_header: function(test) {
-    test.expect(1);
+		test.done();
+	},
+	custom_header: function(test) {
+		test.expect(1);
 
-    var actual = grunt.file.read('tmp/_custom-header.scss');
-    var expected = grunt.file.read('test/expected/custom_header');
-    test.equal(actual, expected, 'A custom header should have been used');
+		var actual = grunt.file.read('tmp/_custom-header.scss');
+		var expected = grunt.file.read('test/expected/custom_header');
+		test.equal(actual, expected, 'A custom header should have been used');
 
-    test.done();
-  }
+		test.done();
+	}
 };
